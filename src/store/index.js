391e9b5d110061133e77f0a304335ce62b842923
starkcodex <maneeshemn@gmail.com>
2024-01-24 20:00:19 +0530

    Initialization of frontend and building navbar for invoicely_app.

---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    user: {
+      username: ''
+    },
+    isAuthenticated: false,
+    token: ''
+  },
+  getters: {
+  },
+  mutations: {
+    inititializeStore(state) {
+      if (localStorage.getItem('token')) {
+        state.token = localStorage.getItem('token')
+        state.isAuthenticated = true
+      } else {
+        state.token = ''
+        state.isAuthenticated = false
+      }
+    },
+    setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    },
+    removeToken(state) {
+      state.token = ''
+      state.isAuthenticated = false
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
